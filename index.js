20d59ed184066c7abdb654646984b9e8295fde3c
Zachary Youngdale <zyoungdale@gmail.com>
2022-08-30 17:21:41 -0400

    commit tree 6ca6229bd3ed4593650623c67d29243fc277b3d1

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Zachary",
+    streetAddress: "702 Perrymont Avenue",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
